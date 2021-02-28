3a207bdc3cc3f953f144f36f376eeda935447056
Janine M <68258844+janine-velveteen@users.noreply.github.com>
2021-02-28 17:34:09 -0500

    Added clearInterval and Stop button feature

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ class DrumKit {
 constructor() {
     this.pads = document.querySelectorAll(".pad");
     this.playButton = document.querySelector(".play");
+    this.currentKick = './sounds/kick-classic.wav';
+    this.currentSnare = './sounds/snare-acoustic01.wav';
+    this.currentHihat = './sounds/hihat-acoustic01.wav';
     this.kickAudio = document.querySelector(".kick-sound");
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 150;
+    this.isPlaying = null;
+    this.selects = document.querySelectorAll('select');
 }
 
 activePad() {
@@ -23,12 +28,15 @@ repeat() {
         if(bar.classList.contains('active')){
 //Check each sound
 if(bar.classList.contains("kick-pad")) {
+    this.kickAudio.currentTime = 0;
     this.kickAudio.play();
 }
 if(bar.classList.contains("snare-pad")) {
+    this.snareAudio.currentTime = 0;
     this.snareAudio.play();
 }
 if(bar.classList.contains("hihat-pad")) {
+    this.hihatAudio.currentTime = 0;
     this.hihatAudio.play();
 }
         }
@@ -37,13 +45,49 @@ if(bar.classList.contains("hihat-pad")) {
 }
 start() {
     const interval = (60/this.bpm) * 1000;
-    setInterval(() => {
+    //Check if it's playing
+    if(!this.isPlaying){
+    this.isPlaying = setInterval(() => {
         this.repeat();
 }, interval);
+    } else {
+        //Clear the interval
+        clearInterval(this.isPlaying);
+        this.isPlaying = null;
+    }
+}
+updateButton(){
+    if(!this.isPlaying) {
+        this.playButton.innerText = "Stop";
+        this.playButton.classList.add('active');
+    } else {
+        this.playButton.innerText = "Play";
+        this.playButton.classList.remove ('active');
+    }
+}
+changeSound(e){
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+    switch(selectionName){
+        case "kick-select":
+            this.kickAudio.src = selectionValue;
+            break;
+            case "snare-select":
+            this.snareAudio.src = selectionValue;
+            break;
+            case "hihat-select":
+            this.hihatAudio.src = selectionValue;
+            break;
+
+
+    }
+
 }
 }
 
 const drumKit = new DrumKit();
+
+//Event Listeners
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener("click", drumKit.activePad);
@@ -54,5 +98,12 @@ drumKit.pads.forEach(pad => {
 });
 
 drumKit.playButton.addEventListener("click", function() { 
+    drumKit.updateButton();
     drumKit.start();
+});
+
+drumKit.selects.forEach(select => {
+select.addEventListener("change", function(e){
+    drumKit.changeSound(e);
+});
 });
